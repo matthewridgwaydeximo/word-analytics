2f0129d22287c65d7266fd3b0ada4581f56ba0c7
Matthew <matthewridgway.deximo@gmail.com>
2024-02-25 19:38:01 +0800

    WIP - Added a placeholder and disabled spell check for text area component

--- src/Components/TextArea.jsx
+++ src/Components/TextArea.jsx
@@ -3,7 +3,7 @@ import Stats from "./Stats";
 export default function TextArea() {
     return (
         <>
-            <textarea className="textarea" />
+            <textarea className="textarea" placeholder="Enter your text here..." spellCheck={false} />
             <Stats />
         </>
     );
