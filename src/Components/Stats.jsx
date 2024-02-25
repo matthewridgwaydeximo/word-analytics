79cdc835dd47cb450c1b4d52eb47ce093aa08354
Matthew <matthewridgway.deximo@gmail.com>
2024-02-25 21:30:29 +0800

    WIP Created Stat component

--- src/Components/Stats.jsx
+++ src/Components/Stats.jsx
@@ -1,3 +1,19 @@
 export default function Stats() {
-    return <section className="stats"></section>;
+    return (
+        <section className="stats">
+            <Stat label="Words" />
+            <Stat label="Characters" />
+            <Stat number={280} label="Instagram" />
+            <Stat number={2200} label="Facebook" />
+        </section>
+    );
+}
+
+function Stat({ number = 0, label = "" }) {
+    return (
+        <section className="stat">
+            <span className="stat__number">{number}</span>
+            <h2 className="second-heading">{label}</h2>
+        </section>
+    );
 }
