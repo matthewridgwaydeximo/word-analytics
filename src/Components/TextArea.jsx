bc75429b8040775ca24d8ed0cc6da2c3597e0b6f
Matthew <matthewridgway.deximo@gmail.com>
2024-02-25 20:10:05 +0800

    WIP - Added function to handle onChange event

--- src/Components/TextArea.jsx
+++ src/Components/TextArea.jsx
@@ -1,9 +1,17 @@
+import { useState } from "react";
 import Stats from "./Stats";
 
 export default function TextArea() {
+    const [text, setText] = useState("");
+
+    const handleOnChanged = (e) => {
+        const { value } = e.target;
+        setText(value);
+    };
+
     return (
         <>
-            <textarea className="textarea" placeholder="Enter your text here..." spellCheck={false} />
+            <textarea value={text} onChange={handleOnChanged} className="textarea" placeholder="Enter your text here..." spellCheck={false} />
             <Stats />
         </>
     );
